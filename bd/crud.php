e3c98cd647287f56dc36e974aa78ffb9e6abb332
WillianAbrego <willianantonioabrego@gmail.com>
2022-03-26 07:19:20 -0600

    editar-borrar

--- bd/crud.php
+++ bd/crud.php
@@ -5,19 +5,47 @@ $conexion = $objeto->Conectar();
 
 //recepcion de los datos enviados mediante post desde js
 
-$nombre=(isset($_POST['nombre'])) ? $_POST['nombre'] : '';
-$pais=(isset($_POST['pais'])) ? $_POST['pais'] : '';
-$edad=(isset($_POST['edad'])) ? $_POST['edad'] : '';
+$nombre = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
+$pais = (isset($_POST['pais'])) ? $_POST['pais'] : '';
+$edad = (isset($_POST['edad'])) ? $_POST['edad'] : '';
+$opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+$id = (isset($_POST['id'])) ? $_POST['id'] : '';
 
-$consulta = "INSERT INTO personas(nombre, pais, edad) VALUES ('$nombre','$pais','$edad')";
-$resultado = $conexion->prepare($consulta);
-$resultado->execute();
+switch ($opcion) {
+    case 1:
+        $consulta = "INSERT INTO personas(nombre, pais, edad) VALUES ('$nombre','$pais','$edad')";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
 
-$consulta = "SELECT id,nombre, pais, edad  from personas ORDER BY id DESC LIMIT 1";
-$resultado = $conexion->prepare($consulta);
-$resultado->execute();
+        $consulta = "SELECT id,nombre, pais, edad  from personas ORDER BY id DESC LIMIT 1";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
 
-$data=$resultado->fetchALL(PDO::FETCH_ASSOC);
+        $data=$resultado->fetchALL(PDO::FETCH_ASSOC);
+        break;
+   
+    case 2:
+        $consulta = "UPDATE  personas SET nombre='$nombre', pais='$pais', edad='$edad' where id='$id' ";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+
+        $consulta = "SELECT id,nombre, pais, edad  from personas where id='$id'";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+
+        $data=$resultado->fetchALL(PDO::FETCH_ASSOC);
+        break;
+   
+    case 3:
+        $consulta = "DELETE FROM personas WHERE id='$id'";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+
+        break;
+   
+}
+
+
+
 print json_encode($data, JSON_UNESCAPED_UNICODE); //enviar el array final en formato json
-
 $conexion=null;
--- main.js
+++ main.js
@@ -34,12 +34,53 @@ $(document).ready(function () {
     $(".modal-title").text("Nueva Persona");
     $("#modalCRUD").modal("show");
     id = null;
+    opcion = 1;
+  });
+
+  var fila; //capturar la fila para editar o borraar el registro
+
+  //boton editar
+  $(document).on("click", ".btnEditar", function () {
+    fila = $(this).closest("tr");
+    id = parseInt(fila.find("td:eq(0)").text());
+    nombre = fila.find("td:eq(1)").text();
+    pais = fila.find("td:eq(2)").text();
+    edad = parseInt(fila.find("td:eq(3)").text());
+
+    $("#nombre").val(nombre);
+    $("#pais").val(pais);
+    $("#edad").val(edad);
+    opcion = 2; //editar
+
+    $(".modal-header").css("background-color", "#007bff");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Editar Persona");
+    $("#modalCRUD").modal("show");
+  });
+
+  //boton borrar
+  $(document).on("click", ".btnBorrar", function () {
+    fila = $(this);
+    id = parseInt($(this).closest("tr").find("td:eq(0)").text());
+    opcion = 3; //borrar
+    var respuesta = confirm(
+      "¿Esta seguro de eliminar el registro: " + id + "?"
+    );
+    if (respuesta) {
+      $.ajax({
+        url: "bd/crud.php",
+        type: "POST",
+        dataType: "json",
+        data: { opcion: opcion, id: id },
+        success: function () {
+          tablaPersonas.row(fila.parents("tr")).remove().draw();
+        },
+      });
+    }
   });
 
   $("#formPersonas").submit(function (e) {
     e.preventDefault();
-
-    id = $.trim($("#id").val());
     nombre = $.trim($("#nombre").val());
     pais = $.trim($("#pais").val());
     edad = $.trim($("#edad").val());
@@ -47,14 +88,17 @@ $(document).ready(function () {
       url: "bd/crud.php",
       type: "POST",
       dataType: "json",
-      data: { nombre: nombre, pais: pais, edad: edad, id: id },
+      data: { nombre: nombre, pais: pais, edad: edad, id: id, opcion: opcion },
       success: function (data) {
-        //var datos = JSON.parse(data);
         id = data[0].id;
         nombre = data[0].nombre;
         pais = data[0].pais;
         edad = data[0].edad;
-        tablaPersonas.row.add([id, nombre, pais, edad]).draw();
+        if (opcion == 1) {
+          tablaPersonas.row.add([id, nombre, pais, edad]).draw();
+        } else {
+          tablaPersonas.row(fila).data([id, nombre, pais, edad]).draw();
+        }
       },
     });
     $("#modalCRUD").modal("hide");
